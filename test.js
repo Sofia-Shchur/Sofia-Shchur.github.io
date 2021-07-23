3988dd90fd177460e2fda7d24b4d59fe31430686
Sofia-Shchur <86024075+Sofia-Shchur@users.noreply.github.com>
2021-07-23 14:36:16 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
 setTimeout(function(){
+  var $_GET = [];
+window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (a, name, value) {
+    $_GET[name] = value;
+});
+
+//get the 'index' query parameter
+if (typeof $_GET['color'] !== 'undefined') {
+console.log($_GET['color'])
+}
+  
   $(".search-field").css( "border", "4px dotted green");
   console.log(1234);console.log(letterA);}, 5000);
