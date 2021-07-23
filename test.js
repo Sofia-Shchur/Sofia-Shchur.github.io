4fcbca2275f8e25c17fe40930ab762350f13a998
Sofia-Shchur <86024075+Sofia-Shchur@users.noreply.github.com>
2021-07-23 14:12:42 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 setTimeout(function(){
   $(".search-field").css( "border", "4px dotted green");
-  console.log(123);console.log(letterA);}, 5000);
+  console.log(1234);console.log(letterA);}, 5000);
