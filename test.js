249ad68ae3636e68b09cefbe51046136e8de1d8c
Sofia-Shchur <86024075+Sofia-Shchur@users.noreply.github.com>
2021-07-26 13:45:52 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,36 @@
+
+if(window.location.href.indexOf("sozdat-otkrytku") >= 0){
+  
+  console.log("we understand what is the special card page")
+  
+}
+
+/*
+
+window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (a, name, value) {
 setTimeout(function(){
   console.log(1);
   var $_GET = [];
-window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (a, name, value) {
-    $_GET[name] = value;
-});
+
+  
+  
+  
+  if (typeof $_GET['mode'] !== 'undefined' && $_GET['mode']) {
+    
+    
+  }
 
 //get the 'index' query parameter
 if (typeof $_GET['color'] !== 'undefined') {
+  
+
   console.log('new');
 console.log($_GET['color'])
   $("#myABC").html("<b>" + $_GET['color'] + "</b> <i>123</i>")
 }
   
   $(".search-field").css( "border", "4px dotted green");
+  
   console.log(letterA);}, 5000);
+  */
 
