436bcd1fb7ff2da37d5e82510caaf520d008f3e1
Sofia-Shchur <86024075+Sofia-Shchur@users.noreply.github.com>
2021-07-26 13:18:49 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-asetTimeout(function(){
+setTimeout(function(){
   console.log(1);
   var $_GET = [];
 window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (a, name, value) {
@@ -14,3 +14,4 @@ console.log($_GET['color'])
   
   $(".search-field").css( "border", "4px dotted green");
   console.log(letterA);}, 5000);
+
