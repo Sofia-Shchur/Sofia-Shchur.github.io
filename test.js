ad1d5e2a69d9cb990b9fe811cf3a893c0b80c5d8
Sofia-Shchur <86024075+Sofia-Shchur@users.noreply.github.com>
2021-07-26 13:14:06 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-setTimeout(function(){
+asetTimeout(function(){
   console.log(1);
   var $_GET = [];
 window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (a, name, value) {
@@ -7,8 +7,9 @@ window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (a, name, value
 
 //get the 'index' query parameter
 if (typeof $_GET['color'] !== 'undefined') {
+  console.log('new');
 console.log($_GET['color'])
-  $("#myABC").html($_GET['color'])
+  $("#myABC").html("<b>" + $_GET['color'] + "</b> <i>123</i>")
 }
   
   $(".search-field").css( "border", "4px dotted green");
